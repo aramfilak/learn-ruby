af0bd0e9d2d65bb8b97c79f00315ca0a0d6a5c78
aramfilak <aram.filak@outlook.de>
2024-09-26 19:56:50 +0200

    time

---
+++ date_and_time.rb
@@ -0,0 +1,38 @@
+# Date and Time
+# Date and Time are a part of the Ruby Standard Library.
+# You can use them to work with dates and times in Ruby.
+
+# current time
+puts  Time.now
+
+# specific time
+puts Time.new(2021, 12, 25, 12, 0, 0)
+
+# Date Arithmetic with Time
+def calculate_my_age(day,month,year)
+  dob = Time.new(year, month, day)
+  now = Time.now
+  age = now.year - dob.year
+  age -= 1 if now.yday < dob.yday
+  puts "You are #{age} years old"
+end
+
+calculate_my_age(25, 12, 1990) # You are 33 years old
+
+ # Formatting Time with strftime
+# %d - day of the month
+# %m - month
+# %Y - year
+# %H - hour
+# %M - minute
+# %S - second
+german_time = Time.now.strftime("%d.%m.%Y %H:%M:%S")
+puts german_time # 25.12.2021 12:00:00
+
+
+def get_the_date_of_date(date)
+  if date.is_a?(Date)
+    date.
+  end
+  yield
+end
