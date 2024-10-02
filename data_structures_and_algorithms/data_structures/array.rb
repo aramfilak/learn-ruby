2b13f1454c2f6bc9f8d66feb8442a250449aada7
aramfilak <aram.filak@outlook.de>
2024-10-03 00:09:29 +0200

    expand array

--- data_structures_and_algorithms/data_structures/array.rb
+++ data_structures_and_algorithms/data_structures/array.rb
@@ -5,7 +5,6 @@ puts arr.length
 # array is a collection of elements
 # cause ruby is a written in C, the array is behind the scenes a fixed size array
 
-
 # if size is fixed (fixed mean you can not change the size) how ruby can append elements to the array?
 # Ruby will create a new array with a new size and copy the elements from the old array to the new array
 # the creation is as follows:
@@ -30,4 +29,63 @@ arr.each do |el|
   puts "no element" if el.nil?
 end
 
+# Example of dynamic array
+class DynamicArray
+  def initialize(size = 16)
+    @size = size
+    @array = Array.new(size)
+    @length = 0
+  end
+
+  def push(element)
+    if @size == @length
+      expand
+    end
+    @array[@length] = element
+    @length += 1
+  end
+
+  def to_s
+    @array.to_s
+  end
+
+  private
+
+  def expand
+    @size *= 2
+    new_array = Array.new(@size)
+    @array.each_with_index do |element, index|
+      new_array[index] = element
+    end
+    @array = new_array
+  end
+end
+
+require 'test/unit'
+
+class TestDynamicArray < Test::Unit::TestCase
+  def test_initialization
+    array = DynamicArray.new
+    assert_equal(16, array.instance_variable_get(:@size))
+    assert_equal(0, array.instance_variable_get(:@length))
+  end
+
+  def test_push
+    array = DynamicArray.new(2)
+    array.push(1)
+    array.push(2)
+    assert_equal("[1, 2]", array.to_s)
+    array.push(3)
+    assert_equal("[1, 2, 3, nil]", array.to_s)
+  end
+
+  def test_expand
+    array = DynamicArray.new(2)
+    array.push(1)
+    array.push(2)
+    array.push(3)
+    assert_equal(4, array.instance_variable_get(:@size))
+    assert_equal("[1, 2, 3, nil]", array.to_s)
+  end
+end
 
