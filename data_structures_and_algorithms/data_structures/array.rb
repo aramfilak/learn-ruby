34575e87624569a3273a89d54d3be69701998fa7
aramfilak <aram.filak@outlook.de>
2024-10-03 14:55:48 +0200

    allow negative index

--- data_structures_and_algorithms/data_structures/array.rb
+++ data_structures_and_algorithms/data_structures/array.rb
@@ -46,7 +46,7 @@ class DynamicArray
   end
 
   def insert(element, index)
-    raise "Array index out of bounds" if index >= @length || index < 0
+    index = abs_index(index)
 
     if @size == @length
       expand
@@ -64,6 +64,11 @@ class DynamicArray
     @array.to_s
   end
 
+  def at(index)
+    index = abs_index(index)
+    @array[index]
+  end
+
   private
 
   def expand
@@ -74,6 +79,15 @@ class DynamicArray
     end
     @array = new_array
   end
+
+  def abs_index(index)
+    if index < 0
+      index += @length
+    end
+    raise "Array index out of bounds" if index >= @length || index < 0
+    index
+  end
+
 end
 
 require 'test/unit'
@@ -109,7 +123,17 @@ class TestDynamicArray < Test::Unit::TestCase
     array.push(2)
     array.insert(3, 1)
     assert_equal("[1, 3, 2, nil]", array.to_s)
-    assert_raise(RuntimeError) { array.insert(4, -1) }
+    assert_raise(RuntimeError) { array.insert(4, -6) }
+  end
+
+  def test_negative_index
+    array = DynamicArray.new(2)
+    array.push(1)
+    array.push(2)
+    assert_equal(2, array.at(-1))
+    assert_equal(1, array.at(-2))
+    assert_raise(RuntimeError) { array.at(-3) }
   end
 end
+
 
